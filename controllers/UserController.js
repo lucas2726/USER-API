bda4e265f27d8de2db487f2cb9dd61862a3c346f
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-12-07 10:57:17 -0400

    ww

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,30 +1,77 @@
 const User = require("../models/User")
 
 class UserController {
-    async index(req, res) {}
+    async index(req, res) {
+        let users = await User.findAll()
+        res.json(users)
+    }
+
+    async findUser(req, res) {
+        let id = req.params.id
+        let user = await User.findById(id)
+        if (user == undefined) {
+            res.status(404)
+            res.json(user)
+        } else {
+            res.status(200)
+            res.json(user)
+        }
+    }
 
     async create(req, res) {
-        let {email, name, password} = req.body
+        let { email, name, password } = req.body
 
-        if(email == undefined) {
+        if (email == undefined) {
             res.status(400)
-            res.json({err: "O e-mail é inválido"})
+            res.json({ err: "O e-mail é inválido" })
             return //Sempre usar o return quando for trabalhar com controller. Pq dai já encerra na hora o programa quando der erro
         }
-//verificação de email para confirmar a existencia
+        //verificação de email para confirmar a existencia
         let emailExists = await User.findEmail(email)
 
-        if(emailExists) {
+        if (emailExists) {
             res.status(406)
-            res.json({err: "O email já está cadastrado!"})
+            res.json({ err: "O email já está cadastrado!" })
             return
         }
 
-        await User.new(email,password,name) //Para cadastrar o email
- 
+        await User.new(email, password, name) //Para cadastrar o email
+
         res.status(200)
         res.send("Tudo ok!")
     }
+
+    async edit(req, res) {
+        let { id, name, role, email } = req.body
+        let result = await User.update(id, email, name, role)
+        if (result != undefined) {
+            if (result.status) {
+                res.status(200)
+                res.send("Tudo OK!")
+            } else {
+                res.status(406)
+                res.send(result.err)
+            }
+        } else {
+            res.status(406)
+            res.send("Ocorreu um erro no servidor!")
+        }
+    }
+
+    async remove(req, res) {
+        let id = req.params.id
+
+        let result = await User.delete(id)
+
+        if (result.status) {
+            res.status(200)
+            res.send("Tudo OK!")
+        } else {
+            res.status(406)
+            res.send(result.err)
+        }
+    }
+
 }
 
 module.exports = new UserController()
--- models/User.js
+++ models/User.js
@@ -2,30 +2,111 @@ const knex = require("../database/connection")
 const bcrypt = require("bcrypt")
 
 class User {
-async new(email, password, name) {
-    try {
-        let hash = await bcrypt.hash(password, 10)
 
-        await knex.insert({email, password: hash, name, role:0}).table("users")
-    } catch (error) {
-        console.log(error)
-    }
-}
-async findEmail(email) { //Para validar o email
-    try {
-        let result = await knex.select("*").from("users").where({email: email}) 
-
-        if (result.length > 0) {
-         return true
-        } else {
-         return false
+    async findAll() {
+        try {
+            let result = await knex.select(["id", "email", "role", "name"]).table("users")
+            return result
+        } catch (err) {
+            console.log(err)
+            return []
         }
-
-    } catch (error) {
-        console.log(error)
-        return false
     }
-}
+
+    async findById(id) {
+        try {
+            let result = await knex.select(["id", "email", "role", "name"]).where({ id: id }).table("users")
+
+            if (result.length > 0) {
+                return result[0]
+            } else {
+                return undefined
+            }
+
+        } catch (err) {
+            console.log(err)
+            return undefined
+        }
+    }
+
+    async new(email, password, name) {
+        try {
+            let hash = await bcrypt.hash(password, 10)
+
+            await knex.insert({ email, password: hash, name, role: 0 }).table("users")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async findEmail(email) { //Para validar o email
+        try {
+            let result = await knex.select("*").from("users").where({ email: email })
+
+            if (result.length > 0) {
+                return true
+            } else {
+                return false
+            }
+
+        } catch (error) {
+            console.log(error)
+            return false
+        }
+    }
+
+    async update(id, email, name, role) {
+
+        let user = await this.findById(id)
+
+        if (user != undefined) {
+
+            let editUser = {}
+
+            if (email != undefined) {
+                if (email != user.email) {
+                    let result = await this.findEmail(email)
+                    if (result == false) {
+                        editUser.email = email
+                    } else {
+                        return { status: false, err: "O email já está cadastrado" }
+                    }
+                }
+            }
+
+            if (name != undefined) {
+                editUser.name = name
+            }
+
+            if (role != undefined) {
+                editUser.role = role
+            }
+
+            try {
+                await knex.update(editUser).where({ id: id }).table("users")
+                return { status: true }
+            } catch (err) {
+                return { status: false, err: err }
+            }
+
+        } else {
+            return { status: false, err: "O usuário não existe" }
+        }
+    }
+
+    async delete(id){
+        let user = await this.findById(id)
+        if (user != undefined) {
+            try {
+                await knex.delete().where({ id: id }).table("users")
+                return {status: true}
+            } catch (err) {
+                return { status: false, err: err }
+            }
+        }else {
+            return { status: false, err: "O usuário não existe, portanto não pode ser deletado." }
+        }
+    }
+
 }
 
 module.exports = new User()
